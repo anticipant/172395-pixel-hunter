1d655a0f5f87d33532b3a47b497a2bbcefc8d36e
Dzmitry Shakhovich <shakhovich.d@yandex.ru>
2018-05-31 21:51:08 +0300

    переименовал переменные; использую шаблонные строки

--- js/main.js
+++ js/main.js
@@ -3,23 +3,23 @@
 const RIGHT_ARROW = 39;
 const LEFT_ARROW = 37;
 const arrows = document.createElement(`div`);
-document.querySelector(`body`).appendChild(arrows).innerHTML = `<div class="arrows__wrap">` +
-  `<style>` +
-  `.arrows__wrap {` +
-  `position: absolute;` +
-  `top: 95px;` +
-  `left: 50%;` +
-  `margin-left: -56px;` +
-  `}` +
-  `.arrows__btn {` +
-  `background: none;` +
-  `border: 2px solid black;` +
-  `padding: 5px 20px;` +
-  `}` +
-  `</style>` +
-  `<button class="arrows__btn"><-</button>` +
-  `<button class="arrows__btn">-></button>` +
-  `</div>`;
+document.querySelector(`body`).appendChild(arrows).innerHTML = `<div class="arrows__wrap">
+  <style>
+  .arrows__wrap {
+  position: absolute;
+  top: 95px;
+  left: 50%;
+  margin-left: -56px;
+  }
+  .arrows__btn {
+  background: none;
+  border: 2px solid black;
+  padding: 5px 20px;
+  }
+  </style>
+  <button class="arrows__btn"><-</button>
+  <button class="arrows__btn">-></button>
+  </div>`;
 const mainCentral = document.querySelector(`main.central`);
 const selectSlide = (element) => {
   mainCentral.innerHTML = ``;
@@ -29,29 +29,29 @@ const screens = [];
 document.querySelectorAll(`template`).forEach((ind)=>{
   screens.push(ind.content);
 });
-let current = 0;
-const select = (index) => {
+let orderNumberOfNextSlide = 0;
+const checkAndShowFollowingSlide = (index) => {
   index = index < 0 ? screens.length - 1 : index;
   index = index >= screens.length ? 0 : index;
-  current = index;
-  selectSlide(screens[current]);
+  orderNumberOfNextSlide = index;
+  selectSlide(screens[orderNumberOfNextSlide]);
 };
 
 document.addEventListener(`keydown`, (evt) => {
   switch (evt.keyCode) {
     case RIGHT_ARROW:
-      select(current + 1);
+      checkAndShowFollowingSlide(orderNumberOfNextSlide + 1);
       break;
     case LEFT_ARROW:
-      select(current - 1);
+      checkAndShowFollowingSlide(orderNumberOfNextSlide - 1);
       break;
   }
 });
 document.querySelector(`.arrows__btn`).addEventListener(`click`, () => {
-  select(current - 1);
+  checkAndShowFollowingSlide(orderNumberOfNextSlide - 1);
 });
 document.querySelector(`.arrows__btn + .arrows__btn`).addEventListener(`click`, () => {
-  select(current + 1);
+  checkAndShowFollowingSlide(orderNumberOfNextSlide + 1);
 });
 
-select(1);
+checkAndShowFollowingSlide(1);
