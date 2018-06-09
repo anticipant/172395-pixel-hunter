e4e64416a5d3e92e0d221a16b15e6f63792ad626
Dzmitry Shakhovich <shakhovich.d@yandex.ru>
2018-06-09 16:55:56 +0300

    timer is ready

--- js/timer.js
+++ js/timer.js
@@ -1,1 +1,18 @@
+export const createTimer = (time) => {
+  if (typeof time !== `number`) {
+    throw new Error(`Wrong type Expect number type`);
+  }
+  if (!Number.isInteger(time)) {
+    throw new Error(`Time should be integer`);
+  }
+  if (time < 0) {
+    throw new Error(`Wrong time Expect more then 0`);
+  }
 
+  return {
+    time,
+    tick() {
+      return createTimer(time - 1);
+    }
+  };
+};
