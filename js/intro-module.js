e65459ab07c1cb76d3c5e0bac860bd5c44b0f735
Dzmitry Shakhovich <shakhovich.d@yandex.ru>
2018-06-21 09:44:51 +0300

    сделал экраны: начальный, приветствия и правил статичными

--- js/intro-module.js
+++ js/intro-module.js
@@ -1,17 +1,15 @@
-import {introData} from './data.js';
 import {renderScreen, changeScreen} from './util.js';
 import greetingScreenElement from './greeting-module.js';
 
-const introScreenMarkup = (state) =>
-  `
+const introScreenMarkup = `
   <div id="main" class="central__content">
     <div id="intro" class="intro">
       <h1 class="intro__asterisk">*</h1>
-      <p class="intro__motto"><sup>*</sup> ${state.footnote}</p>
+      <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
     </div>
   </div>`;
 
-const introScreenElement = renderScreen(introScreenMarkup(introData));
+const introScreenElement = renderScreen(introScreenMarkup);
 const showScreenTrigger = introScreenElement.querySelector(`.intro__asterisk`);
 showScreenTrigger.addEventListener(`click`, () => {
   changeScreen(greetingScreenElement);
--- js/rules-module.js
+++ js/rules-module.js
@@ -1,11 +1,10 @@
-import {rulesData} from './data.js';
 import {renderScreen, changeScreen} from './util.js';
 import showFirstGame from './game-1-module.js';
 
 function checkInputValue(targetInput) {
   return targetInput.value.length;
 }
-const rulesScreenMarkup = (state) => `<header class="header">
+const rulesScreenMarkup = `<header class="header">
     <div class="header__back">
       <button class="back">
         <img src="img/arrow_left.svg" width="45" height="45" alt="Back">
@@ -14,14 +13,22 @@ const rulesScreenMarkup = (state) => `<header class="header">
     </div>
   </header>
   <div class="rules">
-    <h1 class="rules__title">${state.title}</h1>
-    <p class="rules__description">${state.description}</p>
+    <h1 class="rules__title">Правила</h1>
+    <p class="rules__description">Угадай 10 раз для каждого изображения фото <img
+      src="img/photo_icon.png" width="16" height="16"> или рисунок <img
+      src="img/paint_icon.png" width="16" height="16" alt="">.<br>
+      Фотографиями или рисунками могут быть оба изображения.<br>
+      На каждую попытку отводится 30 секунд.<br>
+      Ошибиться можно не более 3 раз.<br>
+      <br>
+      Готовы?
+    </p>
     <form class="rules__form">
-      <input class="rules__input" type="text" placeholder="${state.placeholder}">
-      <button class="rules__button  continue" type="submit" disabled>${state.submitText}</button>
+      <input class="rules__input" type="text" placeholder="Ваше Имя">
+      <button class="rules__button  continue" type="submit" disabled>Go!</button>
     </form>
   </div>`;
-const rulesScreenElement = renderScreen(rulesScreenMarkup(rulesData));
+const rulesScreenElement = renderScreen(rulesScreenMarkup);
 const rulesInput = rulesScreenElement.querySelector(`.rules__input`);
 rulesInput.addEventListener(`keyup`, (evt) => {
   showScreenTrigger.disabled = !(checkInputValue(evt.target) > 0);
